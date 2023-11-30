47572b0e52e87e51c61e7a00e20193a7de555623
jpelegrino1010 <julio.pelegrino@viseo.com>
2023-11-29 23:43:11 -0400

    Basic Client server

---
+++ src/main/java/com/julioluis/javanetworking/Server.java
@@ -0,0 +1,34 @@
+package com.julioluis.javanetworking;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+
+public class Server {
+
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket=new ServerSocket(9999);
+        System.out.println("wainting for client");
+        Socket socket= serverSocket.accept();
+
+        System.out.println("Client connected");
+        InputStream in=socket.getInputStream();
+        OutputStream out=socket.getOutputStream();
+
+        byte buffer []=new byte[1024];
+        in.read(buffer);
+
+        System.out.println("Received from client:: "+ new String(buffer).trim());
+
+        out.write("Hello from server".getBytes());
+
+        socket.close();
+        serverSocket.close();
+
+
+
+    }
+}
