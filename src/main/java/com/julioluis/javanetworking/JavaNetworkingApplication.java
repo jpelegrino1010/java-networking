d3b54d9f9213afaab485b14b8dae3d9874033777
jpelegrino1010 <julio.pelegrino@viseo.com>
2023-11-29 23:13:09 -0400

    First commit

---
+++ src/main/java/com/julioluis/javanetworking/JavaNetworkingApplication.java
@@ -0,0 +1,13 @@
+package com.julioluis.javanetworking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaNetworkingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaNetworkingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/julioluis/javanetworking/JavaNetworkingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.julioluis.javanetworking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaNetworkingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
